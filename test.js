75d85957b3351df784887ce28cba9f887490ac49
rememberwz <rememberwz@gmail.com>
2017-08-19 23:26:36 +0800

    feat: 自动修改模板引用文件吗

--- test.js
+++ test.js
@@ -1,9 +1,6 @@
 let wxml2axml = require('./index')
-let str = `<template name="gotopBtn">
-<view class="gotopBtn" bindtap="gototop" wx:if="{{top!==0}}">
-    <img src="../../image/goback_top.png">
-</view>
-</template>
+let str = `<import src="../../template/score.axml"/>
+<import src="../../template/tags.axml"/>
 `
 let out = wxml2axml.compiler(str);
 
